409a813e6ae63bd386ffe0f87ba130bd61ab49a1
Jharvichu <jharvichu@hotmail.com>
2026-05-15 11:02:57 -0500

    main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,24 +5,20 @@ from app.database import Base, engine
 
 app = FastAPI(title="Mi Proyecto API")
 
-# 1. Define qué orígenes (frontends) tienen permiso de conectarse
 origins = [
-    "http://localhost:5173",  # El puerto por defecto de Vite/React
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
-# 2. Agrega el middleware a la aplicación
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,          # Permite nuestro frontend
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],            # Permite todos los métodos (GET, POST, PUT, DELETE)
-    allow_headers=["*"],            # Permite todos los headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Rutas de incidentes
 app.include_router(reportes.router, prefix="/api/reportes", tags=["Reportes"])
-
 
 @app.get("/")
 def read_root():
